27de25b1648b626cd85647d6016b21681047e406
tsai <lovez81427@gmail.com>
2023-11-17 14:37:54 +0800

    增加條件參數來提高函式的功能性

--- db.php
+++ db.php
@@ -1,14 +1,14 @@
 <?php
-$rows=all('dept');
+$rows=all('students'," where dept='1'");
 
 dd($rows);
 
-function all($table=null){
+function all($table=null,$where=''){
     $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     $pdo=new PDO($dsn,'root','');
 
     if(isset($table) && !empty($table)){
-        $sql="select * from `$table` ";
+        $sql="select * from `$table` $where ";
         $rows=$pdo->query($sql)->fetchALL();
         return $rows;
     }else{
