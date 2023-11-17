cce542fc4c33316c64b513d2096ae82587897387
tsai <lovez81427@gmail.com>
2023-11-17 11:45:09 +0800

    自訂函式預設值用法

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ function sum($a,$b,){
 ?>
 
 
-<h2>不定參數用法</h2>
+<h1>不定參數用法</h1>
 <?php
 function sum2(...$arg){
     return array_sum($arg);
@@ -27,10 +27,27 @@ function sum2(...$arg){
 $result = sum2(1,2);
 echo "結果: " . $result;
 echo "<hr>";
-$result = sum2(38,45,24);
+$result = sum2(38,45,86);
 echo "結果: " . $result;
 echo "<hr>";
 $result = sum2(98,54,23,45,25,63,45,6,78);
 echo "結果: " . $result;
+echo "<hr>";
+?>
+
+
+<h1>自訂函式預設值</h1>
+<?php
+function sum3($a,$b,$c=9){
+    $sum=($a+$b)*$c;
+    echo "$a ` $b , 倍數 $c <br>";
+    return $sum;
+}
+
+echo "總和是".sum3(25,36,54);
+echo "<hr>";
+echo "總和是".sum3(36,89,71);
+echo "<hr>";
+echo "總和是".sum3(25,36,67);
 echo "<hr>";
 ?>
